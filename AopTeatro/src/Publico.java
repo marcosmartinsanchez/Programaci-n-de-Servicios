3aa0294f5731b23a1dca0866a28ced9da06e227c
marcosmarsanch15 <marcos.martinsanchez24@campusfp.es>
2025-11-18 09:04:30 +0100

    Add files via upload

---
+++ AopTeatro/src/Publico.java
@@ -0,0 +1,21 @@
+public class Publico {
+    public void sentarse() {
+        System.out.println("El público se está sentando");
+    }
+
+    public void aCasa() {
+        System.out.println("El público se marcha a sus casas");
+    }
+
+    public void descanso() {
+        System.out.println("El público está saliendo al bar. Vamos a recaudar dinerito");
+    }
+
+    public void aplaudir() {
+        System.out.println("Bravo!! Maravilloso!! Clap clap Clap clap");
+    }
+
+    public void abuchear() {
+        System.out.println("Malos!! Ha sido horrible!! Porque no he traído tomates que si no...");
+    }
+}
